3090512120bb0f110ba61e9086ffb202eebcac60
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-06 16:25:20 +0200

    chap 14 - les jsx

---
+++ premier-projet-react/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur..
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
