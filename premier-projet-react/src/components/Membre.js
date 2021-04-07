5bf895a7a56a6dc01f102037edfcff42d523d422
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-08 00:08:38 +0200

    chap 27 - Utiliser le style inline - Fini

--- premier-projet-react/src/components/Membre.js
+++ premier-projet-react/src/components/Membre.js
@@ -3,7 +3,11 @@ import React, { Fragment } from "react";
 const Membre = ({ nom, age, children }) => {
   return (
     <Fragment>
-      <h2>{nom.toUpperCase()} : { age } </h2>
+      <h2 style={{ 
+        backgroundColor: age < 10 ? 'blue' : 'purple', 
+        color: age < 10 ? 'grey' : 'white' }}>
+        {nom.toUpperCase()} : {age}{" "}
+      </h2>
       {children ? <p>{children}</p> : <Fragment />}
     </Fragment>
   );
