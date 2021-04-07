133b14e20db85a906e9f14a9f6b907b2b666cb78
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-07 23:08:15 +0200

    chap 22 - gestion des evenements - Fini

--- premier-projet-react/src/App.js
+++ premier-projet-react/src/App.js
@@ -1,54 +1,53 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import "./App.css";
-import Membre from './components/Membre'
+import Membre from "./components/Membre";
 
 const famille = {
   membre1: {
-    nom: 'Nicolas',
-    age: 35
+    nom: "Nicolas",
+    age: 35,
   },
   membre2: {
-    nom: 'Caroline',
-    age: 35
+    nom: "Caroline",
+    age: 35,
   },
   membre3: {
-    nom: 'Inès',
-    age: 7
+    nom: "Inès",
+    age: 7,
   },
   membre4: {
-    nom: 'Elise',
-    age: 1
+    nom: "Elise",
+    age: 1,
   },
   membre5: {
-    nom: 'Hugo',
-    age: 1
+    nom: "Hugo",
+    age: 1,
   },
-}
+};
 
 class App extends Component {
   state = {
-    famille
-  }
+    famille,
+  };
+
+  handleClick = () => console.log('Click');
 
   render() {
-    const { titre } = this.props
-    const { famille } = this.state
+    const { titre } = this.props;
+    const { famille } = this.state;
     return (
       <div className="App">
-        <h1>{ titre }</h1>
+        <h1>{titre}</h1>
         {/* <a href="https://www.google.fr/" target="blank">test lien</a> */}
-        <Membre nom={famille.membre1.nom}/>
-        <Membre nom={famille.membre2.nom}/>
-        <Membre nom={famille.membre3.nom}/>
-        <Membre nom={famille.membre4.nom}/>
-        <Membre nom={famille.membre5.nom}>
-          Je suis un garçon.
-        </Membre>
+        <Membre nom={famille.membre1.nom} />
+        <Membre nom={famille.membre2.nom} />
+        <Membre nom={famille.membre3.nom} />
+        <Membre nom={famille.membre4.nom} />
+        <Membre nom={famille.membre5.nom}>Je suis un garçon.</Membre>
+        <button onClick={this.handleClick}>Vieillir</button>
       </div>
     );
   }
 }
 
 export default App;
-
- 
