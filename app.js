3be5e3a615d70be76d84f9123fdab978a28aa4be
Marquillier <nicolas.marquillier@gmail.com>
2021-06-17 14:05:57 +0200

    Chap 19 - A first Summary

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: 'Master vue and build amazing app!',
       vueLink:'https://vuejs.org/'
+    }
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if ( randomNumber < 0.5) {
+        return this.courseGoalA
+      } else {
+        return this.courseGoalB
+      }
     }
   }
 });
