ea16a8c579dcb7212a3f25dc8a1d4040f962508d
Marquillier <nicolas.marquillier@gmail.com>
2021-06-18 11:56:48 +0200

    Chap 25 - Locking Content with v-once

--- udemy-vuejs-49h/basics-03-events-starting-code/app.js
+++ udemy-vuejs-49h/basics-03-events-starting-code/app.js
@@ -1,10 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name
+    },
+    submitForm() {
+      // lors du submit, vueJS recherche la page et on perd les info contenu dans les compteur
+      // en utilisant un event avec la methode preventDefault() on informe à Vue qu'on prend la main sur la methode
+      // ne pas oublier de passer l'event dans les parametre de la fonction
+      // event.preventDefault();
+      alert('Sumitted !!!')
+    },
+    setName(event) {
+      this.name = event.target.value
+    },
     increment(num) {
       this.counter = this.counter + num
     },
